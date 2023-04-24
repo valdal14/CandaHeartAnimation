a1356c5def88ebeea33877ce1fc8278162a3a448
Valerio DAlessio <u105550@canda.com>
2023-04-24 14:30:41 +0200

    switched to main actor the animation completion

--- Sources/CandaHeartAnimation/CandaHeartAnimation.swift
+++ Sources/CandaHeartAnimation/CandaHeartAnimation.swift
@@ -5,10 +5,10 @@ public struct CandaHeartAnimation: View {
 	
 	// MARK: - Properties
 	@ObservedObject var vm: CandaHeartViewModel
-	private let onTap: () async -> Void
+	private let onTap: () async -> (@MainActor ()-> Void) -> Void
 	
 	// MARK: - Init
-	public init(vm: CandaHeartViewModel, onTap: @escaping () async -> Void) {
+	public init(vm: CandaHeartViewModel, onTap: @escaping () async -> (@MainActor ()-> Void) -> Void) {
 		self.vm = vm
 		self.onTap = onTap
 	}
@@ -66,7 +66,21 @@ public struct CandaHeartAnimation: View {
 		generator.prepare()
 		generator.impactOccurred()
 		
-		await onTap()
+		let endProcess = await onTap()
+		endProcess(completeAnimation)
+		
+	}
+	
+	private func completeAnimation() {
+		Task {
+			await MainActor.run {
+				withAnimation {
+					vm.hearts = []
+				}
+				// generate new hearts to repopulate the array
+				vm.generateHearts()
+			}
+		}
 	}
 }
 
@@ -76,6 +90,6 @@ public struct CandaHeartAnimation_Previews: PreviewProvider {
 	public static var previews: some View {
 		CandaHeartAnimation(vm: .init(heartButtonSize: 25,
 									  heartAnimationDuration: 0.8,
-									  numberOfHeartToAnimate: 4), onTap: { })
+									  numberOfHeartToAnimate: 4), onTap: { return {_ in } })
 	}
 }
